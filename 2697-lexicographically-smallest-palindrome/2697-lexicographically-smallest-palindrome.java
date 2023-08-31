2f014b020868fd175fa2dda5a2debc7b99403d9d
noobCoderggez <143007117+noobCoderggez@users.noreply.github.com>
2023-08-31 23:24:00 +0600

    Time: 7 ms (85.78%), Space: 44.2 MB (66.82%) - LeetHub

--- 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.java
+++ 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.java
@@ -1,34 +1,15 @@
 class Solution {
     public String makeSmallestPalindrome(String s) {
-    char a[]=s.toCharArray();
-    
-    int start=0;
-    int end=a.length-1;
-    
-    while(start<end){
-      if(a[start]!=a[end])
-        if(a[start]<a[end]){
-        a[end]=a[start];
-        start++;
-        end--;
-      }
-      else{
-        a[start]=a[end];
-        start++;
-        end--;
-      }
-      
-      if(a[start]==a[end]){
-        start++;
-        end--;
-      }
-    }
-    
-    String str="";
-    for(int i=0;i<a.length;i++){
-     str=str+String.valueOf(a[i]);
-    }
-    
-   return str;
+        char arr[] = s.toCharArray();
+        int i = 0;
+        int j = arr.length - 1;
+        while (i < j) {
+            if (arr[i] < arr[j]) {
+                arr[j--] = arr[i++];
+            } else {
+                arr[i++] = arr[j--];
+            }
+        }
+    return new String(arr);
     }
 }
